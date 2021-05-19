cc6ce5cd9d5daf1c734f4dca762e18df9e3a73ca
Martin Podloucký <podlouckymartin@gmail.com>
2021-05-19 23:05:00 +0200

    Seat component.

--- src/components/SeatPicker/index.jsx
+++ src/components/SeatPicker/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Seat } from '../Seat';
 
 export const SeatPicker = ({ seats }) => {
   return (
     <div>
       <h3>Vyberte sedadlo</h3>
+      <Seat number="15" />
       <ul>
         {seats.map((isOccupied, i) => (
           <li key={i}>
