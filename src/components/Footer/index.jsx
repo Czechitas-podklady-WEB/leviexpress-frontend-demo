6b03db8184281b31e73847fb21c50ac7d7981cef
Martin Podloucký <podlouckymartin@gmail.com>
2021-05-20 08:15:02 +0200

    Footer styling.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import './style.css';
 
-export const Footer = () => <footer>© 2021</footer>;
+export const Footer = () => (
+  <footer>
+    <div className="container">
+      <div className="footer-title">
+        LeviExpress
+      </div>
+      <div className="copyright">
+        Czechitas Digitální akademie: Web, © 2021
+      </div>
+    </div>
+  </footer>
+)
