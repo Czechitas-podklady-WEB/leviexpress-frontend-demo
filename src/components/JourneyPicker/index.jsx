cc0bb37fa95da0c872d927ea9523853d5e758068
Martin Podloucký <podlouckymartin@gmail.com>
2021-05-27 18:32:17 +0200

    JourneyPicker code cleanup.

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -18,12 +18,30 @@ const DatesOptions = ({ dates }) => (
   <>
     <option value="">Vyberte</option>
     {dates.map((date) => (
-      <option key={date}>{date}</option>
+      <option key={date.dateBasic} value={date.dateBasic}>
+        {date.dateExtended}
+      </option>
     ))}
   </>
 );
 
 export const JourneyPicker = ({ onJourneyChange }) => {
+  const [cities, setCities] = useState([]);  
+  const [dates, setDates] = useState([]);
+  const [fromCity, setFromCity] = useState('');
+  const [toCity, setToCity] = useState('');
+  const [date, setDate] = useState('');
+  
+  useEffect(() => {
+    fetch(`${apiBaseUrl}/cities`)
+      .then((response) => response.json())
+      .then((json) => setCities(json.data));
+    
+    fetch(`${apiBaseUrl}/dates`)
+      .then((response) => response.json())
+      .then((json) => setDates(json.data));
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -39,24 +57,6 @@ export const JourneyPicker = ({ onJourneyChange }) => {
       });
   };
 
-  const [fromCity, setFromCity] = useState('');
-  const [toCity, setToCity] = useState('');
-  const [date, setDate] = useState('');
-
-  const [cities, setCities] = useState([]);
-  useEffect(() => {
-    fetch(`${apiBaseUrl}/cities`)
-      .then((response) => response.json())
-      .then((data) => setCities(data.data));
-  }, []);
-
-  const [dates, setDates] = useState([]);
-  useEffect(() => {
-    fetch(`${apiBaseUrl}/dates`)
-      .then((response) => response.json())
-      .then((data) => setDates(data.data));
-  }, []);
-
   return (
     <div className="journey-picker container">
       <h2 className="journey-picker__head">Kam chcete jet?</h2>
@@ -67,7 +67,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
             <select
               value={fromCity}
               onChange={(event) => setFromCity(event.target.value)}
-              >
+            >
               <CityOptions cities={cities} />
             </select>
           </label>
@@ -76,7 +76,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
             <select
               value={toCity}
               onChange={(event) => setToCity(event.target.value)}
-              >
+            >
               <CityOptions cities={cities} />
             </select>
           </label>
@@ -85,12 +85,15 @@ export const JourneyPicker = ({ onJourneyChange }) => {
             <select
               value={date}
               onChange={(event) => setDate(event.target.value)}
-              >
+            >
               <DatesOptions dates={dates} />
             </select>
           </label>
           <div className="journey-picker__controls">
-            <button className="btn" type="submit" disabled={!fromCity || !toCity || !date}>
+            <button 
+              className="btn" 
+              type="submit" 
+              disabled={fromCity === '' || toCity === '' || date === ''}>
               Vyhledat spoj
             </button>
           </div>
